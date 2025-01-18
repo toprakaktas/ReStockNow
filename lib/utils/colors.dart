9554f693a86067cf646ec0ffde19fa79fdc569ce
Toprak <topraktas19@hotmail.com>
2025-01-18 04:10:11 +0300

    Colors added

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -5,3 +5,6 @@ const Color lightBackground = Color(0xAEFFFFFF);
 const Color darkButton = Color(0xD56019B0);
 const Color lightButton = Color(0xF3843BD8);
 const Color warningColor = Color(0xFFE03800);
+const Color blueButton = Color(0xFF3B82F6);
+const Color backButton = Color(0xFF494949);
+const Color backButton2 = Color(0xFFBCBCBC);
